a18700a2c0abc8c7b9473e3cd0fa4934f13a8662
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2024-12-17 13:28:04 -0300

    ok

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,6 +1,9 @@
 #Aqui realizamos la importación de las librerías necesarias
 from django.urls import path
 from .views import registro_view, login_view, logout_view, perfil, actualizar_perfil, eliminar_cuenta
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 # Nombre de la aplicación
 app_name = 'nsusuario'
@@ -26,4 +29,9 @@ urlpatterns = [
     
     # Esta es la Ruta para eliminar la cuenta del usuario
     path('eliminar_cuenta/', eliminar_cuenta, name='eliminar_cuenta'),  # usuario/eliminar_cuenta.html
+    # Otras rutas de la aplicación usuario...
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='usuario/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='usuario/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='usuario/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='usuario/password_reset_complete.html'), name='password_reset_complete'),
 ]
