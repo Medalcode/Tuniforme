c5aae570d50fd9a29459fbdb89e6c3f3d758ba0a
Jonatthan Medalla <152304407+Tryh4rdCode@users.noreply.github.com>
2024-12-18 17:21:58 -0300

    ok

--- coreapi/models.py
+++ coreapi/models.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from tienda.models import Producto
 
+
+#Es el modelo producto de la app tienda 
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
